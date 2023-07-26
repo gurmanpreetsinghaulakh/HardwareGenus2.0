6ca17726c2d6fefed47ec8f45e290410657935f7
Sukhmanpreet <sukhmanaulakh54@gmail.com>
2023-07-26 12:07:07 +0530

    images optimized

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+      domains: ['cdn.mos.cms.futurecdn.net','cdn.vox-cdn.com','cdn.wccftech.com','s.yimg.com'], // Add the domain here
+    },
+  };
--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -5,22 +5,23 @@ import RealtedNews from '../../../Components/RealtedNews'
 import TrendingDown from '../../../Components/TrendingDown'
 import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
 
 
 
 export async function generateMetadata({ params: { slug } }) {
 
-const slugMeta = await getData();
-const decodedTitle = decodeURIComponent(slug)
-const decodedSlug = decodeURIComponent(slug).toLocaleLowerCase();
-// console.log(decodedSlug)
+  const slugMeta = await getData();
+  const decodedTitle = decodeURIComponent(slug)
+  const decodedSlug = decodeURIComponent(slug).toLocaleLowerCase();
+  // console.log(decodedSlug)
 
-const filteredArticles = slugMeta.articles.find((article) =>
-article.title.toLocaleLowerCase() === decodedSlug)
+  const filteredArticles = slugMeta.articles.find((article) =>
+    article.title.toLocaleLowerCase() === decodedSlug)
 
   return {
     title: decodedTitle,
-    description: filteredArticles?.paraphrased_content?.slice(0,400),
+    description: filteredArticles?.paraphrased_content?.slice(0, 400),
     keywords: filteredArticles?.meta_tags,
     viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
   }
@@ -32,10 +33,10 @@ async function getData() {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data')
   }
- 
+
   return res
 }
- 
+
 export default async function Page({ params: { slug } }) {
   const data = await getData()
 
@@ -43,25 +44,25 @@ export default async function Page({ params: { slug } }) {
   // console.log(decodedSlug)
 
   const filteredArticles = data.articles.find((article) =>
-  article.title.toLocaleLowerCase() === decodedSlug
-);
+    article.title.toLocaleLowerCase() === decodedSlug
+  );
 
 
 
-// console.log(filteredArticles)
+  // console.log(filteredArticles)
 
 
-const article = filteredArticles
- 
-return (
+  const article = filteredArticles
 
-  
-  <>
-  <Head>
+  return (
+
+
+    <>
+      <Head>
         <meta property="og:description" content={article?.description} />
       </Head>
 
-    {article ? (
+      {article ? (
         <div className="news-page">
           <div className="news-page-content">
             <div className="news-header">
@@ -75,10 +76,18 @@ return (
             </div>
             <div className="trending_down">
               <div className="news-content">
-                <img src={article.urlToImage} alt="Article" className="article-image" />
-                <p>"{article.description.slice(0, 112)} --- <Link style={{color:'coral'}} href={article.url}>{article.source.name}</Link> </p>
+                <Image
+                  src={article.urlToImage}
+                  alt="Article"
+                  className="article-image"
+                  layout="responsive"
+                  width={100}
+                  height={550}
+                  objectFit="cover"
+                />
+                <p>"{article.description.slice(0, 112)} --- <Link style={{ color: 'coral' }} href={article.url}>{article.source.name}</Link> </p>
                 {article.paraphrased_content && typeof article.paraphrased_content === "string" ? (
-                  <p>{article.paraphrased_content.slice(0, 1600)} -- <Link style={{color:'coral'}} href={article.url}>Continue Reading.</Link></p>
+                  <p>{article.paraphrased_content.slice(0, 1600)} -- <Link style={{ color: 'coral' }} href={article.url}>Continue Reading.</Link></p>
                 ) : (
                   <p>Content not available.</p>
                 )}
@@ -100,13 +109,13 @@ return (
             </div>
           </div>
         </div>
-      
-    ) : (
-      <p style={{color:'white', display:'flex', justifyContent:'center', alignItems: 'center', marginTop:200 , fontSize: 20}}>No articles found.</p>
-    )}
-    <RealtedNews newsData={data} />
-  </>
-);
+
+      ) : (
+        <p style={{ color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 200, fontSize: 20 }}>No articles found.</p>
+      )}
+      <RealtedNews newsData={data} />
+    </>
+  );
 }
 
 
